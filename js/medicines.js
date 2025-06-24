27471ac90d3320e393ece554425c08ab78039e46
kavita355321 <136420398+kavita355321@users.noreply.github.com>
2025-06-24 10:45:07 +0530

    Added medicines.js just your medicine data array

---
+++ js/medicines.js
@@ -0,0 +1,24 @@
+const medicines = [
+  {
+    id: "med1",
+    name: "Paracetamol",
+    description: "Used for fever and pain relief",
+    price: 20,
+    image: "images/medicine1.jpg"
+  },
+  {
+    id: "med2",
+    name: "Ibuprofen",
+    description: "Anti-inflammatory and painkiller",
+    price: 40,
+    image: "images/medicine2.jpg"
+  },
+  {
+    id: "med3",
+    name: "Cough Syrup",
+    description: "Soothes dry and wet cough",
+    price: 60,
+    image: "images/medicine3.jpg"
+  }
+];
+
